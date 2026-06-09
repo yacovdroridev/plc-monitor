b1500320c72e0535d6cd0d9d788e3bd19d673bf3
Yacov Drori <yacovdrori@gmail.com>
2026-06-09 12:00:53 +0300

    Add --port option for viewer; datetime.utcnow deprecation warning removed

--- viewer/app.py
+++ viewer/app.py
@@ -73,4 +73,9 @@ def clear():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument("--host", default="0.0.0.0")
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=True)
